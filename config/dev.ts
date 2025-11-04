51f09047d44bef9f526716f72967bbb7c6a90104
guozhenjiang <guozhenjiang@pin-dao.cn>
2025-11-04 14:49:20 +0800

    api bug修复

--- config/dev.ts
+++ config/dev.ts
@@ -9,9 +9,9 @@ export default {
   // 注入环境变量到应用中
   defineConstants: {
     'process.env.API_BASE_URL': JSON.stringify(ENV_CONFIG.API_BASE_URL),
-    'process.env.API_TIMEOUT': ENV_CONFIG.API_TIMEOUT,
-    'process.env.DEBUG': ENV_CONFIG.DEBUG,
-    'process.env.ENABLE_MOCK': ENV_CONFIG.ENABLE_MOCK,
+    'process.env.API_TIMEOUT': JSON.stringify(ENV_CONFIG.API_TIMEOUT),
+    'process.env.DEBUG': JSON.stringify(ENV_CONFIG.DEBUG),
+    'process.env.ENABLE_MOCK': JSON.stringify(ENV_CONFIG.ENABLE_MOCK),
     'process.env.LOG_LEVEL': JSON.stringify(ENV_CONFIG.LOG_LEVEL),
     'process.env.CDN_URL': JSON.stringify(ENV_CONFIG.CDN_URL),
     'process.env.WS_URL': JSON.stringify(ENV_CONFIG.WS_URL),
